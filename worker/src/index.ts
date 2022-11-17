51aa2a0f37e72c5e1b1247ac51c41178d9c95dac
lucianobajr <luciano.alcantara@ufv.br>
2022-11-16 22:04:14 -0300

    add(+): base to workers

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,30 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Redis } from "@upstash/redis";
+import { Router } from "worktop";
+import { listen } from "worktop/cache";
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+const routes = new Router();
+
+declare global {
+  var UPSTASH_REDIS_REST_URL: string;
+  var UPSTASH_REDIS_REST_TOKEN: string;
 }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+const redis = new Redis({
+  url: UPSTASH_REDIS_REST_URL,
+  token: UPSTASH_REDIS_REST_TOKEN,
+});
+
+routes.add("GET", "/get", async (req, res) => {
+  const my_replenisher = await redis.get("my_replenisher");
+  return res.send(200, { my_replenisher });
+});
+
+routes.add("POST", "/change", async (req, res) => {
+  const my_replenisher = await redis.incr("my_replenisher");
+  return res.send(201, { my_replenisher });
+});
+
+listen(routes.run);
